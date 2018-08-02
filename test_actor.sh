db841f36029f46fa618b7738064fdaad3d2d834f
loyave <loyaveforever@gmail.com>
2018-08-02 13:46:48 +0800

    rm .sh

--- test_actor.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES='' python experiment.py --job_name=actor --task=0 \
-          --num_actors=1
--- test_single_machine.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python experiment.py --num_actors=2 --batch_size=32
